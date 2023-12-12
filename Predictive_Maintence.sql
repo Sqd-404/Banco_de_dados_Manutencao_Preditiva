00f1c4d7ce9f709b07c638baca4ce4c558e1758f
Sownatta <wilsonn.rocha@outlook.com>
2023-12-11 22:03:37 -0300

    Atualizando modelo e script, separando arquivo de consultas

--- Predictive_Maintence.sql
+++ Predictive_Maintence.sql
@@ -2,10 +2,19 @@ CREATE DATABASE IF NOT EXISTS Predictive_Maintence;
 
 USE Predictive_Maintence;
 
+CREATE TABLE IF NOT EXISTS modelo_maquina(
+	modelID TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(20),
+    tamanho_motor DECIMAL(6, 2) DEFAULT NULL,
+    marca VARCHAR(20) DEFAULT NULL,
+    localizacao VARCHAR(20) DEFAULT NULL      
+);
+
 CREATE TABLE IF NOT EXISTS maquina (
     machineID SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    model VARCHAR(20),
-    age TINYINT UNSIGNED
+    modelID TINYINT UNSIGNED,
+    age TINYINT UNSIGNED,
+    FOREIGN KEY (modelID) REFERENCES modelo_maquina(modelID)
 );
 
 CREATE TABLE IF NOT EXISTS comp (
@@ -41,11 +50,10 @@ CREATE TABLE IF NOT EXISTS erro (
 CREATE TABLE IF NOT EXISTS falha (
     date_time DATETIME,
     machineID SMALLINT UNSIGNED,
-    compID TINYINT UNSIGNED,
-    failure VARCHAR(10),
+    failure_comp TINYINT UNSIGNED,
     FOREIGN KEY (machineID) REFERENCES maquina(machineID),
-    FOREIGN KEY (compID) REFERENCES comp(compID),
-    PRIMARY KEY (date_time, machineID, failure)
+    FOREIGN KEY (failure_comp) REFERENCES comp(compID),
+    PRIMARY KEY (date_time, machineID, failure_comp)
 );
 
 CREATE TABLE IF NOT EXISTS manutencao (
@@ -59,41 +67,36 @@ CREATE TABLE IF NOT EXISTS manutencao (
 
 ----------------------------------------------------------------
 
+INSERT INTO modelo_maquina (nome) VALUES
+('model1'),
+('model2'),
+('model3'),
+('model4');
+
 INSERT INTO erro_tipo (nome) VALUES
 ('error1'),
 ('error2'),
 ('error3'),
-('error4'),
-('error5'),
-('error6'),
-('error7'),
-('error8'),
-('error9'),
-('error10');
+('error4');
 
 INSERT INTO comp (nome) VALUES
 ('comp1'),
 ('comp2'),
 ('comp3'),
 ('comp4'),
-('comp5'),
-('comp6'),
-('comp7'),
-('comp8'),
-('comp9'),
-('comp10');
+('comp5');
 
-INSERT INTO maquina (model, age) VALUES
-('model1', 18),
-('model2', 7),
-('model3', 8),
-('model4', 7),
-('model5', 2),
-('model6', 7),
-('model7', 20),
-('model8', 16),
-('model9', 7),
-('model10', 10);
+INSERT INTO maquina (modelID, age) VALUES
+(1, 18),
+(1, 7),
+(3, 8),
+(4, 7),
+(4, 2),
+(1, 7),
+(2, 20),
+(2, 16),
+(3, 7),
+(1, 10);
 
 INSERT INTO telemetria (date_time, machineID, volt, rotate_, pressure, vibration) VALUES 
 ('2015-01-01 09:00:00', 1, 176.2179, 418.5041, 113.0779, 45.0877),
@@ -113,23 +116,23 @@ INSERT INTO erro (date_time, machineID, errorID) VALUES
 ('2023-06-06 10:00:00', 3, 2),
 ('2023-07-01 11:45:00', 1, 1),
 ('2023-08-02 12:30:00', 2, 4),
-('2023-08-05 13:15:00', 3, 5),
+('2023-08-05 13:15:00', 3, 3),
 ('2023-09-01 14:00:00', 1, 2),
 ('2023-09-08 14:45:00', 2, 1),
 ('2023-11-06 15:30:00', 4, 3),
 ('2023-12-06 16:15:00', 5, 4);
 
-INSERT INTO falha (date_time, machineID, compID, failure) VALUES 
-('2023-01-13 08:30:00', 1, 1, 'comp1'),
-('2023-02-15 09:15:00', 2, 2, 'comp2'),
-('2023-03-22 10:00:00', 3, 3, 'comp3'),
-('2023-04-30 11:45:00', 2, 4, 'comp4'),
-('2023-05-15 12:30:00', 5, 5, 'comp5'),
-('2023-07-10 13:15:00', 3, 6, 'comp6'),
-('2023-09-11 14:00:00', 7, 7, 'comp7'),
-('2023-10-06 14:45:00', 8, 8, 'comp8'),
-('2023-11-08 15:30:00', 3, 9, 'comp9'),
-('2023-11-25 16:15:00', 2, 10, 'comp10');
+INSERT INTO falha (date_time, machineID, failure_comp) VALUES 
+('2023-01-13 08:30:00', 1, 1),
+('2023-02-15 09:15:00', 2, 2),
+('2023-03-22 10:00:00', 3, 3),
+('2023-04-30 11:45:00', 2, 4),
+('2023-05-15 12:30:00', 5, 5),
+('2023-07-10 13:15:00', 3, 2),
+('2023-09-11 14:00:00', 7, 4),
+('2023-10-06 14:45:00', 8, 5),
+('2023-11-08 15:30:00', 3, 1),
+('2023-11-25 16:15:00', 2, 1);
 
 INSERT INTO manutencao (date_time, machineID, compID) VALUES 
 ('2023-01-13 08:30:00', 1, 1),
@@ -137,56 +140,8 @@ INSERT INTO manutencao (date_time, machineID, compID) VALUES
 ('2023-03-22 10:00:00', 3, 3),
 ('2023-04-30 11:45:00', 4, 4),
 ('2023-05-15 12:30:00', 5, 5),
-('2023-07-10 13:15:00', 6, 6),
-('2023-09-11 14:00:00', 7, 7),
-('2023-10-06 14:45:00', 8, 8),
-('2023-11-08 15:30:00', 9, 9),
-('2023-11-25 16:15:00', 10, 10);
-
-----------------------------------------------------------------
-
--- Retorna o(s) modelo(s) com maior número de falhas
-SELECT model, total_falhas
-FROM (
-    SELECT maquina.model, COUNT(*) AS total_falhas
-    FROM maquina
-    JOIN falha ON maquina.machineID = falha.machineID
-    GROUP BY maquina.model
-) AS subquery
-WHERE total_falhas = (
-    SELECT MAX(total_falhas)
-    FROM (
-        SELECT COUNT(*) AS total_falhas
-        FROM maquina
-        JOIN falha ON maquina.machineID = falha.machineID
-        GROUP BY maquina.model
-    ) AS subquery_max
-);
-
--- Retorna o número de falhas por idade
-SELECT maquina.age, COUNT(falha.date_time) AS total_falhas 
-FROM maquina
-LEFT JOIN falha ON maquina.machineID = falha.machineID
-GROUP BY maquina.age
-ORDER BY maquina.age;
-
--- Retorna os componentes que maior número de falhas por máquina
-SELECT maquina.machineID, maquina.model, falha.compID, comp.nome AS nome_componente, COUNT(*) AS total_falhas
-FROM maquina
-JOIN falha ON maquina.machineID = falha.machineID
-JOIN comp ON falha.compID = comp.compID
-GROUP BY maquina.machineID, falha.compID
-ORDER BY maquina.machineID, total_falhas DESC;
-
--- Retorna a idade média das máquinas por modelo
-SELECT model, AVG(age) AS media_idade
-FROM maquina
-GROUP BY model;
-
--- Retorna a quantidade de erro por tipo de erro e modelo da máquina
-SELECT maquina.model, erro_tipo.nome AS tipo_erro, COUNT(*) AS quantidade_erros
-FROM maquina
-JOIN erro ON maquina.machineID = erro.machineID
-JOIN erro_tipo ON erro.errorID = erro_tipo.errorID
-GROUP BY maquina.model, erro_tipo.nome
-ORDER BY maquina.model, quantidade_erros DESC;
+('2023-07-10 13:15:00', 6, 2),
+('2023-09-11 14:00:00', 7, 3),
+('2023-10-06 14:45:00', 8, 1),
+('2023-11-08 15:30:00', 9, 5),
+('2023-11-25 16:15:00', 10, 4);
---
+++ consultas.sql
@@ -0,0 +1,47 @@
+USE Predictive_Maintence;
+
+-- Retorna o(s) modelo(s) com maior número de falhas
+SELECT model, total_falhas
+FROM (
+    SELECT maquina.model, COUNT(*) AS total_falhas
+    FROM maquina
+    JOIN falha ON maquina.machineID = falha.machineID
+    GROUP BY maquina.model
+) AS subquery
+WHERE total_falhas = (
+    SELECT MAX(total_falhas)
+    FROM (
+        SELECT COUNT(*) AS total_falhas
+        FROM maquina
+        JOIN falha ON maquina.machineID = falha.machineID
+        GROUP BY maquina.model
+    ) AS subquery_max
+);
+
+-- Retorna o número de falhas por idade
+SELECT maquina.age, COUNT(falha.date_time) AS total_falhas 
+FROM maquina
+LEFT JOIN falha ON maquina.machineID = falha.machineID
+GROUP BY maquina.age
+ORDER BY maquina.age;
+
+-- Retorna os componentes que maior número de falhas por máquina
+SELECT maquina.machineID, maquina.model, falha.compID, comp.nome AS nome_componente, COUNT(*) AS total_falhas
+FROM maquina
+JOIN falha ON maquina.machineID = falha.machineID
+JOIN comp ON falha.compID = comp.compID
+GROUP BY maquina.machineID, falha.compID
+ORDER BY maquina.machineID, total_falhas DESC;
+
+-- Retorna a idade média das máquinas por modelo
+SELECT model, AVG(age) AS media_idade
+FROM maquina
+GROUP BY model;
+
+-- Retorna a quantidade de erro por tipo de erro e modelo da máquina
+SELECT maquina.model, erro_tipo.nome AS tipo_erro, COUNT(*) AS quantidade_erros
+FROM maquina
+JOIN erro ON maquina.machineID = erro.machineID
+JOIN erro_tipo ON erro.errorID = erro_tipo.errorID
+GROUP BY maquina.model, erro_tipo.nome
+ORDER BY maquina.model, quantidade_erros DESC;
